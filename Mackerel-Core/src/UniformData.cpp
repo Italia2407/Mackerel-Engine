214363b7ffcc7fdf84f9bb8e290b1990948af124
citrus-tree <jtmodisett@gmail.com>
2023-02-16 13:58:23 +0000

    Adjusted both projects to treat warnings as errors, as discussed. Fixed a typo in the UniformData.cpp file that was preventing the engine from building. Added explicit static_casts in places where data types of different precisions were being converted. Unified comment style in UniformData.cpp file.

--- Mackerel-Core/src/UniformData.cpp
+++ Mackerel-Core/src/UniformData.cpp
@@ -1,88 +1,97 @@
 #include "UniformData.h"
 
 namespace MCK {
-UniformData::UniformData(GLuint shaderProgram, std::string name) :
-	uniformName(name), uniformLocation(glGetUniformLocation(shaderProgram, name.c_str())) {}
+UniformData::UniformData(GLuint shaderprogram, std::string name) :
+	uniformName(name), uniformLocation(glGetUniformLocation(shaderprogram, name.c_str())) {}
 
 // uint8
 UInt08Uniform::UInt08Uniform(GLuint shaderprogram, std::string name, uint8_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+	UniformData(shaderprogram, name), value(value) {}
 
 void UInt08Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1ui(uniformLocation, value);
 }
+
 // uint16
 UInt16Uniform::UInt16Uniform(GLuint shaderprogram, std::string name, uint16_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+	UniformData(shaderprogram, name), value(value) {}
 
 void UInt16Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1ui(uniformLocation, value);
 }
+
 // uint32
 UInt32Uniform::UInt32Uniform(GLuint shaderprogram, std::string name, uint32_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+	UniformData(shaderprogram, name), value(value) {}
 
 void UInt32Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1ui(uniformLocation, value);
 }
+
 // uint64 
 UInt64Uniform::UInt64Uniform(GLuint shaderprogram, std::string name, uint64_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+	UniformData(shaderprogram, name), value(value) {}
 
 void UInt64Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
-	glUniform1ui(uniformLocation, value);
+	glUniform1ui(uniformLocation, static_cast<GLuint>(value));
 }
-// int8 uniform
-Int08Uniform::Int08Uniform(GLuint shaderProgram, std::string name, int8_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+
+// int8
+Int08Uniform::Int08Uniform(GLuint shaderprogram, std::string name, int8_t value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void Int08Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1i(uniformLocation, value);
 }
-// int16 uniform
-Int16Uniform::Int16Uniform(GLuint shaderProgram, std::string name, int16_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+
+// int16
+Int16Uniform::Int16Uniform(GLuint shaderprogram, std::string name, int16_t value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void Int16Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1i(uniformLocation, value);
 }
-// int32 Uniform
-Int32Uniform::Int32Uniform(GLuint shaderProgram, std::string name, int32_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+
+// int32
+Int32Uniform::Int32Uniform(GLuint shaderprogram, std::string name, int32_t value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void Int32Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1i(uniformLocation, value);
 }
-// int64 Uniform
-Int64Uniform::Int64Uniform(GLuint shaderProgram, std::string name, int64_t value) :
-	UniformData(shaderProgram, name), value(value) {}
+
+// int64
+Int64Uniform::Int64Uniform(GLuint shaderprogram, std::string name, int64_t value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void Int64Uniform::Bind(AssetType::Material::UniformState& uniformState)
 {
-	glUniform1i(uniformLocation, value);
+	glUniform1i(uniformLocation, static_cast<GLint>(value));
 }
-// float Uniform
-FloatUniform::FloatUniform(GLuint shaderProgram, std::string name, float value) :
-	UniformData(shaderProgram, name), value(value) {}
+
+// float
+FloatUniform::FloatUniform(GLuint shaderprogram, std::string name, float value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void FloatUniform::Bind(AssetType::Material::UniformState& uniformState)
 {
 	glUniform1f(uniformLocation, value);
 }
+
 // double
-DoubleUniform::DoubleUniform(GLuint shaderProgram, std::string name, double value) :
-	UniformData(shaderProgram, name), value(value) {}
+DoubleUniform::DoubleUniform(GLuint shaderprogram, std::string name, double value) :
+	UniformData(shaderprogram, name), value(value) {}
 
 void DoubleUniform::Bind(AssetType::Material::UniformState& uniformState)
 {
-	glUniform1d(shaderProgram, value);
+	glUniform1d(uniformLocation, value);
 }
 
 
