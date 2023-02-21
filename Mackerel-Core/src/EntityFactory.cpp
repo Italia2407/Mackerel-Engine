94e112d247b9c1b688499adc72be1cedfb616c97
citrus-tree <jtmodisett@gmail.com>
2023-02-21 20:02:00 +0000

    Added an object pool and factory for entites.

---
+++ Mackerel-Core/src/EntityFactory.cpp
@@ -0,0 +1,19 @@
+#include "EntityFactory.h"
+
+namespace MCK::EntitySystem
+{
+	Entity* EntityFactory::Get()
+	{
+		return pool.Get();
+	}
+
+	bool EntityFactory::Recycle(Entity* entity)
+	{
+		return pool.Recycle(entity);
+	}
+
+	void EntityFactory::Recall()
+	{
+		pool.Recall();
+	}
+}
---
+++ Mackerel-Core/src/EntityPool.cpp
@@ -0,0 +1,79 @@
+#include "EntityPool.h"
+
+#include "Entity.h"
+
+namespace MCK::EntitySystem::Pooling
+{
+	EntityPool::EntityPool()
+	{
+		Allocate();
+	}
+
+	EntityPool::~EntityPool()
+	{
+		if (live.size() > 0)
+		{
+			// This is very bad!
+			// Don't delete this while there are still live entities!!!
+			// There could be more clean-up here, but in actual use cases this should
+			//     probably cause a soft crash.
+		}
+
+		while (pool.size() > 0)
+		{
+			delete pool.top();
+			pool.pop();
+		}
+	}
+
+	Entity* EntityPool::Get()
+	{
+		Entity* ret = nullptr;
+
+		if (pool.size() > 0)
+			Allocate();
+
+		live.emplace(pool.top());
+		ret = pool.top();
+		pool.pop();
+
+		return ret;
+	}
+
+	bool EntityPool::Recycle(Entity* entity)
+	{
+		bool success = false;
+
+		auto loc = live.find(entity);
+		if (loc == live.end())
+		{
+			success = false;
+		}
+		else
+		{
+			// TODO: reinitialise/reset the data of the instance once this
+			//           functionality is implemented in the entity class.
+			live.erase(loc); // remove from live set
+			pool.push(*loc); // push back to stack
+		}
+
+		return success;
+	}
+
+	void EntityPool::Recall()
+	{
+		for (auto inst : live)
+		{
+			inst->Destroy();
+		}
+	}
+
+	void EntityPool::Allocate(uint32_t count)
+	{
+		for (uint32_t i = 0; i < count; i++)
+		{
+			Entity* newEntity = new Entity;
+			pool.push(newEntity);
+		}
+	}
+}
