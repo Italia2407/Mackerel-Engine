ffe16d8d7e3914ee9712ca1b97d3ba22b3c58266
sc19aep <sc19aep@leeds.ac.uk>
2023-02-22 15:39:22 +0000

    Added Time Manager Class

---
+++ Mackerel-Core/TimeManager.cpp
@@ -0,0 +1,19 @@
+#include "TimeManager.h"
+
+using namespace MCK;
+
+TimeManager* TimeManager::instance = nullptr;
+
+TimeManager::TimeManager()
+{
+
+}
+
+TimeManager::~TimeManager()
+{
+
+}
+
+// private Singleton functions
+
+// public functions
