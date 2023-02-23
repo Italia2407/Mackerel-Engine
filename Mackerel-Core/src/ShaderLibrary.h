be7ab5322737b8f08776b2dfc06cc9d1ce0f4b73
sc19aep <sc19aep@leeds.ac.uk>
2023-02-23 12:43:44 +0000

    Changed the comments to Doxygen style

--- Mackerel-Core/src/ShaderLibrary.h
+++ Mackerel-Core/src/ShaderLibrary.h
@@ -12,10 +12,10 @@ namespace MCK
 		class Shader;
 	}
 
-	/// <summary>
-	/// This class is the resource manager responsible for loading, retrieving, and
-	///		releasing Shaders in memory.
-	/// </summary>
+	/**
+	* This class is the resource manager responsible for loading, retrieving, and
+	*		releasing Shaders in memory.
+	*/
 	class ShaderLibrary
 	{
 	private:
@@ -41,69 +41,77 @@ namespace MCK
 
 		// private implementation of Singleton functions
 
-		/// <summary>
-		/// Private implementation of the ShaderLibrary::Get() function.
-		/// Attempts to retrieve the specified shader from memory.
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <param name="out">: output reference to return the retrieved shader</param>
-		/// <returns>true if the function succeeded or false if it fails</returns>
+		/**
+		 * Private implementation of the ShaderLibrary::Get() function.
+		 * Attempts to retrieve the specified shader from memory.
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \param out: output reference to return the retrieved shader
+		 * \return true if the function succeeded or false if it fails
+		 */
 		bool privGet(ShaderEnum asset, AssetType::Shader* out);
 
-		/// <summary>
-		/// Private implementation of the ShaderLibrary::Load() function.
-		/// Attempts to load the specified shader into memory from disk.
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <param name="filepath">: filepath to the shader</param>
-		/// <returns>true if the function succeeded or false if it fails</returns>
+		/**
+		 * Private implementation of the ShaderLibrary::Load() function.
+		 * Attempts to load the specified shader into memory from disk.
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \param filepath: filepath to the shader
+		 * \return true if the function succeeded or false if it fails
+		 */
 		bool privLoad(ShaderEnum asset, std::string filepath);
 
-		/// <summary>
-		/// Private implementation of the ShaderLibrary::Load() function.
-		/// Attempts to release the specified shader from memory. 
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <returns>true if a shader was released from memory or false if it wasn't
-		///				loaded in the first place.</returns>
+		/**
+		 * Private implementation of the ShaderLibrary::Load() function.
+		 * Attempts to release the specified shader from memory. 
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \return true if a shader was released from memory or false if it wasn't
+		 *		loaded in the first place.
+		 */
 		bool privFree(ShaderEnum asset);
 
 	public:
-		/// <summary>
-		/// <para>Releases all shaders in memory, and frees the ShaderLibrary instance.</para>
-		/// <para>Should only be called at the end of the applications lifetime!</para>
-		/// </summary>
-		/// <returns>true if the library was successfully released, false if it
-		//				wasn't initialised to begin with</returns>
+		/**
+		 * Releases all shaders in memory, and frees the ShaderLibrary instance.
+		 * Should only be called at the end of the applications lifetime!
+		 * 
+		 * \return true if the library was successfully released, false if it
+		 *		wasn't initialised to begin with
+		 */
 		bool Release();
 
-		/// <summary>
-		/// Attempts to retrieve the specified shader from memory.
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <param name="out">: output reference to return the retrieved shader</param>
-		/// <returns>true if the function succeeded or false if it fails</returns>
+		/**
+		 * Attempts to retrieve the specified shader from memory.
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \param out: output reference to return the retrieved shader
+		 * \return true if the function succeeded or false if it fails
+		 */
 		bool Get(ShaderEnum asset, AssetType::Shader* out)
 		{
 			return Instance()->privGet(asset, out);
 		}
 
-		/// <summary>
-		/// Attempts to load the specified shader into memory from disk.
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <returns>true if the function succeeded or false if it fails</returns>
+		/**
+		 * Attempts to load the specified shader into memory from disk.
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \param filepath: filepath to the shader
+		 * \return true if the function succeeded or false if it fails
+		 */
 		bool Load(ShaderEnum asset, std::string filepath)
 		{
 			return Instance()->privLoad(asset, filepath);
 		}
 
-		/// <summary>
-		/// Attempts to release the specified shader from memory. 
-		/// </summary>
-		/// <param name="asset">: enum identifier of the desired shader</param>
-		/// <returns>true if a shader was released from memory or false if it wasn't
-		///				loaded in the first place.</returns>
+		/**
+		 * Attempts to release the specified shader from memory.
+		 * 
+		 * \param asset: enum identifier of the desired shader
+		 * \return true if a shader was released from memory or false if it wasn't
+		 *		loaded in the first place.
+		 */
 		bool Free(ShaderEnum asset)
 		{
 			return Instance()->privFree(asset);
