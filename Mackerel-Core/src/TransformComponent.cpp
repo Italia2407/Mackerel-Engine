a5f262ddce161b0bf7b980180c3efee638ce7c29
IGRecca <igrecca27@outlook.com>
2023-04-01 16:57:29 +0100

    Refactored Transform Component to Support Quaternions & Full 3D Shearing

--- Mackerel-Core/src/TransformComponent.cpp
+++ Mackerel-Core/src/TransformComponent.cpp
@@ -7,87 +7,48 @@
 
 namespace MCK::EntitySystem
 {
-	/**
-	* Returns the transformation matrix of this entity
-	* Takes the transformation of parent entities into account
-	* 
-	* 
-	*/
-	Eigen::Matrix4f TransformComponent::GetTransformationMatrix()
-	{
-		// Get the parent entity's transformation matrix, if it exists.
-		Eigen::Matrix4f parentMat;
-		parentMat.Identity();
-		if (entity->parent != nullptr) {
-			TransformComponent* parentTransform = entity->parent->GetComponent<TransformComponent>();
-			if (parentTransform != nullptr)
-				parentMat = parentTransform->GetTransformationMatrix();
-		}
+/**
+* Returns Transformation Matrix of Entity
+* Takes Parent Entity Transformations into Account
+*/
+Eigen::Matrix4f TransformComponent::GetTransformationMatrix()
+{
+	// Get Parent Entity's Transformation Matrix, if Existstant
+	Eigen::Matrix4f parentMatrix;
+	parentMatrix.Identity();
 
-		// Matrices for the different possible transformations.
-		Eigen::Matrix4f translateMat;
-		translateMat.Identity();
-		translateMat(0,3) = position.x();
-		translateMat(1,3) = position.y();
-		translateMat(2,3) = position.z();
-
-		// Rotation requires a different matrix for all three axes.
-		Eigen::Matrix4f rotateXMat;
-		rotateXMat.Identity();
-		rotateXMat(1,1) = cos(eulerAngles.x());
-		rotateXMat(1,2) = -sin(eulerAngles.x());
-		rotateXMat(2,1) = sin(eulerAngles.x());
-		rotateXMat(2,2) = cos(eulerAngles.x());
-		
-		Eigen::Matrix4f rotateYMat;
-		rotateYMat.Identity();
-		rotateYMat(0,0) = cos(eulerAngles.y());
-		rotateYMat(0,2) = sin(eulerAngles.y());
-		rotateYMat(2,0) = -sin(eulerAngles.y());
-		rotateYMat(2,2) = cos(eulerAngles.y());
-		
-		Eigen::Matrix4f rotateZMat;
-		rotateZMat.Identity();
-		rotateZMat(0,0) = cos(eulerAngles.z());
-		rotateZMat(0,1) = -sin(eulerAngles.z());
-		rotateZMat(1,0) = sin(eulerAngles.z());
-		rotateZMat(1,1) = cos(eulerAngles.z());
-		
-		Eigen::Matrix4f scaleMat;
-		scaleMat.Identity();
-		scaleMat(0,0) = scale.x();
-		scaleMat(1,1) = scale.y();
-		scaleMat(2,2) = scale.z();
-
-		// The shear matrix is dependant on which axis it is being sheared along.
-		Eigen::Matrix4f shearMat;
-		shearMat.Identity();
-		if (shear.x() == 0.f) {
-			shearMat(1,0) = shear.y();
-			shearMat(2,0) = shear.z();
-		}
-		else if (shear.x() == 1.f) {
-			shearMat(0,1) = shear.y();
-			shearMat(2,1) = shear.z();
-		}
-		else if (shear.x() == 2.f) {
-			shearMat(0,2) = shear.y();
-			shearMat(1,2) = shear.z();
-		}
-
-		// The reflect matrix is Boolean.
-		Eigen::Matrix4f reflectMat;
-		reflectMat.Identity();
-		reflectMat(0,0) = (reflect.x() == 1 ? -1.f : 1.f);
-		reflectMat(1,1) = (reflect.y() == 1 ? -1.f : 1.f);
-		reflectMat(2,2) = (reflect.z() == 1 ? -1.f : 1.f);
-		
-		Eigen::Matrix4f mat;
-		// Order matters, so this multiplication not be correct.
-		mat = parentMat * translateMat * rotateXMat * rotateYMat * rotateZMat * scaleMat * shearMat * reflectMat;
-
-		return mat;
+	if (entity->parent != nullptr) {
+		TransformComponent* parentTransform = entity->parent->GetComponent<TransformComponent>();
+		if (parentTransform != nullptr)
+			parentMatrix = parentTransform->GetTransformationMatrix();
 	}
+
+	// Apply Affine Transformations
+	Eigen::Affine3f affineTransformations;
+	affineTransformations.translate(m_Position);
+	affineTransformations.rotate(m_Rotation);
+	affineTransformations.scale(m_Scale);
+
+	// Create Shear Matrix
+	Eigen::Matrix4f shearMatrix;
+	shearMatrix.Identity();
+
+	// Set X-Axis Shear
+	shearMatrix(0, 1) = m_YZPlaneShear.x();
+	shearMatrix(0, 2) = m_YZPlaneShear.y();
+	// Set Y-Axis Shear
+	shearMatrix(1, 0) = m_XZPlaneShear.x();
+	shearMatrix(1, 2) = m_XZPlaneShear.y();
+	// Set Z-Axis Shear
+	shearMatrix(2, 0) = m_XYPlaneShear.x();
+	shearMatrix(2, 1) = m_XYPlaneShear.y();
+
+	Eigen::Matrix4f transformationMatrix;
+	// Order matters, so this multiplication not be correct.
+	transformationMatrix = parentMatrix * affineTransformations.matrix() * shearMatrix;
+
+	return transformationMatrix;
+}
 
 	/**
 	* Returns the type id of this component
@@ -126,38 +87,37 @@ namespace MCK::EntitySystem
 		std::cout << "I have been destroyed" << std::endl;
 	}
 
-	bool TransformComponent::Deserialise(json data)
-	{
-		// Get the entitiy's transform component data.
-		data = data["data"];
+bool TransformComponent::Deserialise(json data)
+{
+	// Get Entity's Transform Component Data.
+	data = data["data"];
 
-		// position is simply the amount by which the object translates.
-		position.x() = data["positionX"];
-		position.y() = data["positionY"];
-		position.z() = data["positionZ"];
+	// Get Transform Component's Position
+	m_Position.x() = data["positionX"];
+	m_Position.y() = data["positionY"];
+	m_Position.z() = data["positionZ"];
 
-		// eulerAngles are the angles in x, y and z by which the object rotates, in radians.
-		eulerAngles.x() = data["angleX"];
-		eulerAngles.y() = data["angleY"];
-		eulerAngles.z() = data["angleZ"];
+	// Get Transform Component's Rotation
+	m_Rotation.x() = data["rotationAxisX"];
+	m_Rotation.y() = data["rotationAxisY"];
+	m_Rotation.z() = data["rotationAxisZ"];
+	m_Rotation.w() = data["rotationAmount"];
 
-		// scale is the factor by which the object scales in the x, y and z axes.
-		scale.x() = data["scaleX"];
-		scale.y() = data["scaleY"];
-		scale.z() = data["scaleZ"];
+	// Get Transform Component's Scale
+	m_Scale.x() = data["scaleX"];
+	m_Scale.y() = data["scaleY"];
+	m_Scale.z() = data["scaleZ"];
 
-		// Shear is a but more complex. The first value indicates along which axes the object is being sheared.
-		// The second two values are the how much it is being sheared by in the other two axes.
-		// These other two axes change depending on the first.
-		shear.x() = data["reflectX"];
-		shear.y() = data["reflectY"];
-		shear.z() = data["reflectZ"];
+	// Get Transform Component's Shear
+	m_YZPlaneShear.x() = data["YZPlaneShearY"];
+	m_YZPlaneShear.y() = data["YZPlaneShearZ"];
 
-		// reflect is three Boolean values of whether the object reflects in the x, y or z axes.
-		reflect.x() = data["reflectX"];
-		reflect.y() = data["reflectY"];
-		reflect.z() = data["reflectZ"];
+	m_XZPlaneShear.x() = data["XZPlaneShearX"];
+	m_XZPlaneShear.y() = data["XZPlaneShearZ"];
 
-		return true;
-	}
+	m_XYPlaneShear.x() = data["XYPlaneShearX"];
+	m_XYPlaneShear.y() = data["XYPlaneShearY"];
+
+	return true;
+}
 }
--- Mackerel-Core/src/TransformComponent.h
+++ Mackerel-Core/src/TransformComponent.h
@@ -1,26 +1,38 @@
 #pragma once
 
-#include "Eigen/Core.h"
+#include "Eigen/Eigen.h"
 #include "Component.h"
 
 namespace MCK::EntitySystem
 {
-	class TransformComponent : public Component
-	{
-	public:
-		Eigen::Vector3f position;
-		Eigen::Vector3f eulerAngles;
-		Eigen::Vector3f scale;
-		Eigen::Vector3f shear;
-		Eigen::Vector3i reflect;
+class TransformComponent : public Component
+{
+private:
+	Eigen::Vector3f m_Position;
+	Eigen::Quaternion<float> m_Rotation;
+	Eigen::Vector3f m_Scale;
 
-		Eigen::Matrix4f GetTransformationMatrix();
+	Eigen::Vector2f m_YZPlaneShear;
+	Eigen::Vector2f m_XZPlaneShear;
+	Eigen::Vector2f m_XYPlaneShear;
 
-		void OnCreate();
-		void OnUpdate();
-		void OnDestroy();
-		bool Deserialise(json data);
+public:
+	Eigen::Matrix4f GetTransformationMatrix();
 
-		TypeInfoRef GetType();
-	};
+	Eigen::Vector3f& Position() { return m_Position; }
+	Eigen::Quaternion<float>& Rotation() { return m_Rotation; }
+	Eigen::Vector3f& Scale() { return m_Scale; }
+
+	Eigen::Vector2f& XAxisShear() { return m_YZPlaneShear; }
+	Eigen::Vector2f& YAxisShear() { return m_XZPlaneShear; }
+	Eigen::Vector2f& ZAxisShear() { return m_XYPlaneShear; }
+
+public:
+	void OnCreate() override;
+	void OnUpdate() override;
+	void OnDestroy() override;
+	bool Deserialise(json data) override;
+
+	TypeInfoRef GetType() override;
+};
 }
