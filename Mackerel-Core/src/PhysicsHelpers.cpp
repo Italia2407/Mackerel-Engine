3b086531402ea4ba48d6b0c60687b9a76105841e
Alex Elliott <sc19a2e@leeds.ac.uk>
2023-04-09 16:52:04 +0100

    Implemented collider creation helper functions.

--- Mackerel-Core/src/PhysicsHelpers.cpp
+++ Mackerel-Core/src/PhysicsHelpers.cpp
@@ -22,5 +22,39 @@ namespace MCK::Physics::PhysicsHelpers
 	{
 		return dynamicsObject->getInternalType() == btCollisionObject::CollisionObjectTypes::CO_RIGID_BODY;
 	}
+
+	btCollisionShape* CreateBoxShape(btVector3 halfExtents)
+	{
+		btBoxShape box = btBoxShape(halfExtents);
+		return static_cast<btCollisionShape*>(box.getUserPointer());
+	}
+
+	btCollisionShape* CreateSphereShape(float radius)
+	{
+		btSphereShape sphere = btSphereShape(radius);
+		return static_cast<btCollisionShape*>(sphere.getUserPointer());
+	}
+
+	btCollisionShape* CreateCapsuleShape(float width, float height)
+	{
+		btCapsuleShape capsule = btCapsuleShape(width, height);
+		return static_cast<btCollisionShape*>(capsule.getUserPointer());
+	}
+
+	void InitialiseCollider(CreateCollisionShapeInfo shapeInfo, btCollisionShape*& collisionShape)
+	{
+		switch (shapeInfo.colliderType)
+		{
+		case (Box):
+			collisionShape = CreateBoxShape(btVector3(shapeInfo.width/2, shapeInfo.height/2, shapeInfo.depth/2));
+			break;
+		case (Sphere):
+			collisionShape = CreateSphereShape(shapeInfo.radius);
+			break;
+		case (Capsule):
+			collisionShape = CreateCapsuleShape(shapeInfo.width, shapeInfo.height);
+			break;
+		};
+	}
 }
 
--- Mackerel-Core/src/PhysicsHelpers.h
+++ Mackerel-Core/src/PhysicsHelpers.h
@@ -31,14 +31,37 @@ namespace MCK::Physics::PhysicsHelpers
 	 */
 	bool IsRigidbody(btCollisionObject* dynamicsObject);
 
-	// TODO Implement me. Commented so it compiles
-	// btCollisionShape* CreateBoxShape(btVector3 halfExtents);
+	/**
+	* Creates a btBoxShape entity and returns a pointer to it as a btCollisionShape.
+	* 
+	* \param halExtents: a btVector3 of half the x. y and z lengths of the box.
+	* \return a pointer to the btCollisionShape.
+	*/
+	btCollisionShape* CreateBoxShape(btVector3 halfExtents);
 
-	// btCollisionShape* CreateSphereShape(float radius);
+	/**
+	* Creates a btSphereShape entity and returns a pointer to it as a btCollisionShape.
+	*
+	* \param radius: the radius of the sphere.
+	* \return a pointer to the btCollisionShape.
+	*/
+	btCollisionShape* CreateSphereShape(float radius);
 
-	// btCollisionShape* CreateCapsuleShape(float width, float height);
+	/**
+	* Creates a btCapsuleShape entity and returns a pointer to it as a btCollisionShape.
+	*
+	* \param width: the width of the capsule.
+	* \param height: the height of the capsule.
+	* \return a pointer to the btCollisionShape.
+	*/
+	btCollisionShape* CreateCapsuleShape(float width, float height);
 
-
-	// TODO Implement me - Create the shape and assign it to collisionShape
-	// void InitialiseCollider(CreateCollisionShapeInfo shapeInfo, btCollisionShape*& collisionShape);
+	/**
+	* Creates a collider using the data in the first parameter and other helper functions.
+	* Assigns the pointer returned from those functions to the second parameter.
+	* 
+	* \param shapeInfo: struct containing the type of the collision shape and its dimensions.
+	* \param collisionShape: a pointer to be used for the new collisionShape.
+	*/
+	void InitialiseCollider(CreateCollisionShapeInfo shapeInfo, btCollisionShape*& collisionShape);
 }
