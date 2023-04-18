496d05d5ff841bae84af3249a19456eeba5564ae
sc19aep <sc19aep@leeds.ac.uk>
2023-04-18 18:31:41 +0100

    Moved function definitions to cpp for audio channel

--- Mackerel-Core/src/AudioChannel.h
+++ Mackerel-Core/src/AudioChannel.h
@@ -11,24 +11,9 @@ namespace MCK::Audio
 		unsigned int id;
 		FMOD::Channel* fChannel;
 
-		// TODO Implement me - commented so it compiles
-		bool IsPlaying()
-		{
-			bool playing = false;
-			fChannel->isPlaying(&playing);
-			return playing;
-		};
+		bool IsPlaying();
 
-		void SetPosition(Eigen::Vector3f position)
-		{
-			// convert to FMOD vector
-			FMOD_VECTOR fVec;
-			fVec.x = position[0];
-			fVec.y = position[1];
-			fVec.z = position[2];
-
-			fChannel->set3DAttributes(&fVec, NULL);
-		};
+		void SetPosition(Eigen::Vector3f position);
 	};
 }
 
