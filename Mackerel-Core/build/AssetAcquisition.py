4e798647a27af480c7089e96215f3b942a467781
citrus-tree <jtmodisett@gmail.com>
2023-05-18 18:15:26 +0100

    .ozz file automation implemented.

--- Mackerel-Core/build/AssetAcquisition.py
+++ Mackerel-Core/build/AssetAcquisition.py
@@ -53,6 +53,12 @@ def build_asset_set(a_root, a_dir, a_file_formats, a_folder_ignores, resource_fo
 				if in_resource_path or len(resource_folder_names) == 0:
 					a_set.append(full_path)
 					print("Adding asset {0}".format(full_path))
+
+					# run ozz converter if necessary
+					if fext == ".gltf" or fext == ".glb":
+						ozz_path = os.path.join(sys.argv[0], os.pardir, "GenerateOzz.py")
+						os.system("python \"{0}\" \"{1}\"".format(ozz_path, full_path))
+
 				else:
 					print(path_split)
 					print(resource_folder_names)
