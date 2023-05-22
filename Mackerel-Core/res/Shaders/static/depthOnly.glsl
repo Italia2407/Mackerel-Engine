059f189e3cc476007435222ba7161190d401885d
IGRecca <igrecca27@outlook.com>
2023-05-22 19:25:26 +0100

    Fixed Shaders Input Missing Type

--- Mackerel-Core/res/Shaders/static/depthOnly.glsl
+++ Mackerel-Core/res/Shaders/static/depthOnly.glsl
@@ -5,6 +5,6 @@ layout(location = 0) in vec3 v2fPosition;
 layout(location = 1) in vec3 v2fNormal;
 layout(location = 2) in vec2 v2fUV;
 layout(location = 3) in vec3 v2fTint;
-layout(location = 4) in v2fCameraViewDirection;
+layout(location = 4) in vec3 v2fCameraViewDirection;
 
 void main() {}
